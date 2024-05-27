9b1d1a5e3fd5c1ad3d5aa33e51aee39ada37d193
zainabsulaiman <zainabseryu@gmail.com>
2024-05-27 13:29:42 +0200

    new

--- src/views/dashboard/photographer/Appoitment.vue
+++ src/views/dashboard/photographer/Appoitment.vue
@@ -56,7 +56,7 @@ import axios from "axios";
 import SectionHeader from "@/components/dashboard/SectionHeader.vue";
 import DataTable from "@/components/DataTable.vue";
 import Modal from '../../../components/modal/Modal.vue';
-import ButtonCta from "@/components/Button/ButtonCta";
+import ButtonCta from "@/components/Button/ButtonCta.vue";
 import { alert } from "@/utils/alertUtils";
 // import moment from "moment";
 
